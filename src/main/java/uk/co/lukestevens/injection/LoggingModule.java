a88e3493f6257d84ba7ab6042a5f57df37190740
Luke Stevens <lcm.stevens96@gmail.com>
2021-01-28 22:07:34 +0000

    Get logging level from config

--- src/main/java/uk/co/lukestevens/injection/LoggingModule.java
+++ src/main/java/uk/co/lukestevens/injection/LoggingModule.java
@@ -8,6 +8,7 @@ import uk.co.lukestevens.config.ApplicationProperties;
 import uk.co.lukestevens.config.Config;
 import uk.co.lukestevens.injection.annotations.EnvConfig;
 import uk.co.lukestevens.jdbc.Database;
+import uk.co.lukestevens.logging.LoggerLevel;
 import uk.co.lukestevens.logging.LoggingProvider;
 import uk.co.lukestevens.logging.provider.ConsoleLoggingProvider;
 import uk.co.lukestevens.logging.provider.DatabaseLoggingProvider;
@@ -24,15 +25,19 @@ public class LoggingModule extends AbstractModule {
 	
 	protected static final String DATABASE_LOGGING_ENABLED_KEY = "database.logging.enabled";
 	protected static final boolean DATABASE_LOGGING_ENABLED_DEFAULT = false;
+	
+	protected static final String LOGGER_LEVEL_KEY = "logging.level";
+	protected static final LoggerLevel LOGGER_LEVEL_DEFAULT = LoggerLevel.DEBUG;
 
 	@Provides
 	@Singleton
 	protected LoggingProvider providesLogging(@EnvConfig Config config, Database db, ApplicationProperties appProperties) {
+		LoggerLevel loggerLevel = config.getParsedValueOrDefault(LOGGER_LEVEL_KEY, LoggerLevel::valueOf, LOGGER_LEVEL_DEFAULT);
 		if(config.getAsBooleanOrDefault(DATABASE_LOGGING_ENABLED_KEY, DATABASE_LOGGING_ENABLED_DEFAULT)) {
-			return new DatabaseLoggingProvider(db, appProperties);
+			return new DatabaseLoggingProvider(db, appProperties, loggerLevel);
 		}
 		else {
-			return new ConsoleLoggingProvider();
+			return new ConsoleLoggingProvider(loggerLevel);
 		}
 	}
 }
